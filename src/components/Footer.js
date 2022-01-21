191078417b44a99b76672e6a6c5b62b3c7eb219d
JZ <73402906+JZSweet@users.noreply.github.com>
2022-01-20 18:42:39 -0800

    add pics to the header element

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
+
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <h2>Footer</h2>
+        <h2></h2>
       </div>
     );
   };
 };
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import Head from './img/Head.png';
+import Jia from './img/Jia.png'
  
 class Header extends Component {
   render() {
     return (
-      <div className="header">
-        <h1>Demo Application</h1>
-        <p>Demo application created in React App</p>
+      <div className='header'>
+            <img className='d-block w-100'
+                 src={Head}
+                 alt='Header img1'
+                />
+
+            <img className='d-block w-100'
+                 src={Jia}
+                 alt='Header img2'
+                />    
       </div>
     );
   };
