151b06a8e93e32850dd018ea9ae2572b984d219f
JZ <73402906+JZSweet@users.noreply.github.com>
2022-01-21 13:22:16 -0800

    add footer img to footer, add id to header and footer pics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Foot from './img/Footer.png';
 
 
 class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        <h2></h2>
+      < div className="footer" >
+        <img className='d-block w-100'
+          id='footerpic'
+          src={Foot}
+          alt='footer img'
+        />
       </div>
     );
   };
