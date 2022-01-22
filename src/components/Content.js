e1cd95bf6d824d89a6f4c55de515698f6c7eae81
JZ <73402906+JZSweet@users.noreply.github.com>
2022-01-22 11:27:20 -0800

    start with about me section

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,8 +4,11 @@ class Content extends Component {
   render() {
     return (
       <div className="main" id='aboutme'>
-
-
+        <h1>About Me</h1>
+        <h2>Hi there,</h2>
+        <h3>my name is JZ (Jiaqian Zhao).</h3>
+        <h4>I am a full-stack web developer and UX/UI designer.</h4>
+        
       </div>
     );
   };
