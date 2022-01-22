1b7bc6040070ca034b5931a72a0a3be62a39ca5e
JZ <73402906+JZSweet@users.noreply.github.com>
2022-01-22 10:36:36 -0800

    changes on content file. add class and id to content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,14 @@ class Footer extends Component {
   render() {
     return (
       <Breadcrumb>
+        <Breadcrumb.Item href="#">Resume</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">LinkedIn</Breadcrumb.Item>
         <div className="footer" >
           <img className='d-block w-100'
             id='footerpic'
             src={Foot}
             alt='footer img' />
         </div>
-        <Breadcrumb.Item href="#">Resume</Breadcrumb.Item>
-        <Breadcrumb.Item href="#">LinkedIn</Breadcrumb.Item>
       </Breadcrumb>
 
     );
