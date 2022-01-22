d3ec7f8a45459813860416b753c3096a08cd8247
JZ <73402906+JZSweet@users.noreply.github.com>
2022-01-22 11:43:30 -0800

    add pic to about me section

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import JZ from './img/JZ.png';
 
 class Content extends Component {
   render() {
     return (
-      <div className="main" id='aboutme'>
-        <h1>About Me</h1>
-        <h2>Hi there,</h2>
-        <h3>my name is JZ (Jiaqian Zhao).</h3>
-        <h4>I am a full-stack web developer and UX/UI designer.</h4>
-        
-      </div>
+      <Container>
+        <Row md={4}>
+          <Col>
+            <img className='d-block w-100'
+              id='jz1'
+              src={JZ}
+              alt='main img1' />
+          </Col>
+          <Col xs={6}>
+            <div className="main" id='aboutme'>
+              <h1>About Me</h1>
+              <h2>Hi there,</h2>
+              <h3>my name is JZ (Jiaqian Zhao).</h3>
+              <h4>I am a full-stack web developer and UX/UI designer.</h4>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   };
 };
