4311f86a00dbf89d709c725b646272acd0ec6b30
JZ <73402906+JZSweet@users.noreply.github.com>
2022-01-27 18:04:10 -0800

    link linkdin to footer and added resume

--- src/components/FooterCrumb.js
+++ src/components/FooterCrumb.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Resume from "./resume/Jiaqian Zhao.pdf";
 import './css/style.css';
 
 class FooterCrumb extends Component {
     render() {
       return (
         <Breadcrumb>
-          <Breadcrumb.Item href='#'>Resume</Breadcrumb.Item>
-          <Breadcrumb.Item href='#'>LinkedIn</Breadcrumb.Item>
+          <Breadcrumb.Item href={Resume} download="JiaqianZhao">Resume</Breadcrumb.Item>
+          <Breadcrumb.Item href='https://www.linkedin.com/in/jiaqian-zhao/'>LinkedIn</Breadcrumb.Item>
         </Breadcrumb>  
       );
     };
